b4d436feef88b2254e9c76f9d4a1d5201df4ba76
Hồ Kim Thiên Nga <127382679+cabungbu@users.noreply.github.com>
2023-06-27 16:09:54 +0700

    50

--- 167_Luu_Do_Thuat_Toan_Python/Bai050/Bai050.py
+++ 167_Luu_Do_Thuat_Toan_Python/Bai050/Bai050.py
@@ -1,10 +1,11 @@
 
-import math
 n = float(input("Nhap n: "))
-s = 0;
-i = 1;
+s = 0
+i = 1
+
 while(i <= n ):
-    if n%i==0:
-        s = s+1
+    if (n%i==0):
+        s = s+i
+    i = i+1
 
 print ("Tong cac uoc so cua n: ", s)
